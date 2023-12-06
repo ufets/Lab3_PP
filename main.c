85969a78011a8c3d518a85557e6d0917de010f29
ufets <maksim78998@gmail.com>
2023-12-07 02:01:29 +0300

    corrected timing()

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #define RED_COLOR "\x1b[31m"
 #define RESET_COLOR "\x1b[0m"
 
-#define SIZE 100
+#define SIZE 1000000
 #define LIMIT 10000
 
 /// @brief Different codes of errors
@@ -30,21 +30,19 @@ void check_openmp_support()
 
 void timing(int *array, int *array2, int size)
 {
-    clock_t start1 = clock();
-    shell_sort(array, size);
-    clock_t end1 = clock();
-    printf("result:\t%lf\n", (double)(end1 - start1) / CLOCKS_PER_SEC);
 
     int num = 100;
     double result = 0;
     for (int i = 1; i <= 12; i++)
     {
         for(int j = 0; j < num; j++)
-        {
+        {   
+            copy_array(array, &array2, size);
             double start = omp_get_wtime();
             shell_sort_parallel(array2, SIZE, i);
             double end = omp_get_wtime();
             result += end - start;
+            free(array2);
         }
         
         printf("%lf\n", result / num);
@@ -59,12 +57,11 @@ int main(int argc, char *argv[])
     srand(time(NULL));
     int *array = create_array(SIZE, LIMIT);
     int *array2 = NULL;
-    copy_array(array, &array2, SIZE);
 
     timing(array, array2, SIZE);
 
     free(array);
-    free(array2);
+
 
     return 0;
 }
